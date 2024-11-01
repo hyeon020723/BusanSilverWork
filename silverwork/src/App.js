aace3e3b22cb5942bfd5a079992327edcc741a3c
imga <dlga7723@gmail.com>
2024-11-01 18:07:25 +0900

    init pages

--- silverwork/src/App.js
+++ silverwork/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import OptionsPage from "./pages/OptionsPage";
+import ResultsPage from "./pages/ResultsPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/options" element={<OptionsPage />} />
+        <Route path="/results" element={<ResultsPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ silverwork/src/pages/OptionsPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function OptionsPage() {
+  return <div>Options Selection Page</div>;
+}
+
+export default OptionsPage;
